6468e8cd160d20f4eae983cfae79a8a9af18e39f
Kevin Funke <funke@validierung.cc>
2024-12-04 11:45:41 +0100

    added nodes to  get the node starting

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,11 @@ server=1
 listen=1
 rpcuser=${KOBO_RPCUSER:-kobocoinrpc}
 rpcpassword=${KOBO_RPCPASSWORD}
+addnode=178.36.237.104
+addnode=217.241.207.232
+addnode=31.50.154.102
+addnode=65.109.3.42
+addnode=84.169.224.238
 EOF
 echo "Created new configuration at ${KOBO_CONF}"
 fi
